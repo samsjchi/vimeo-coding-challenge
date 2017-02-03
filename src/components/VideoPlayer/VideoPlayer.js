4f8ef9b23d71f5faa3c68de2e1ccec5ba70038cd
Sam Chi <samuel.sj.chi@gmail.com>
2017-02-02 19:49:04 -0800

    Add initial styling to video info box and VideoList sidebar

--- src/components/VideoPlayer/VideoPlayer.js
+++ src/components/VideoPlayer/VideoPlayer.js
@@ -14,7 +14,8 @@ const VideoPlayer = ({video}) => {
         <iframe src={videoUrl} width="640" height="360" frameBorder="0" allowFullScreen></iframe>
       </div>
       <div className="video-content">
-        <h1 className="video-info__title">{video.title}</h1>
+        <div className="video-info">
+          <h1 className="video-info__title">{video.title}</h1>
           <div className="video-info__user-icon">
             <a href={video.user_url}><img src={video.user_portrait_medium} alt={video.user_url}></img></a>
           </div>
@@ -22,10 +23,13 @@ const VideoPlayer = ({video}) => {
             <div className="video-info__username">from {video.user_name}</div>
             <div className="video-info__upload-date">published on {video.upload_date}</div>
           </div>
-          <div className="video-info__stats">{video.stats_number_of_plays} plays | {video.stats_number_of_likes} likes | {video.stats_number_of_comments} comments</div>
-      </div>
-      <div className="video-description">
-        {video.description}
+          <div className="video-info__stats">
+            {video.stats_number_of_plays} plays | {video.stats_number_of_likes} likes | {video.stats_number_of_comments} comments
+          </div>
+        </div>
+        <div className="video-description">
+          {video.description}
+        </div>
       </div>
     </div>
   );
