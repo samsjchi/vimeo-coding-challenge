615050bfc829fd4e497638887d211f28895fbe99
Sam Chi <samuel.sj.chi@gmail.com>
2017-02-02 18:12:51 -0800

    Fetch video data from Vimeo API and store in App state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: []
+    };
+  }
+
+  componentDidMount() {
+    this.getVimeoVideos();
+  }
+
+  getVimeoVideos() {
+    fetch('http://vimeo.com/api/v2/channel/staffpicks/videos.json') // Using Fetch API
+      .then(res => res.json())
+      .then(videos => {
+        console.log('videos from parsed Fetch API response:', videos);
+        this.setState({ videos });
+      });
+  }
+
   render() {
     return (
       <div className="App">
